082e661256be977860dca29ced876f0526694add
shiki10masaoka14 <shiki10masaoka14@gmail.com>
2020-08-29 21:46:02 +0900

    layout

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#1f2f54`,
       marginBottom: `1.45rem`,
     }}
   >
